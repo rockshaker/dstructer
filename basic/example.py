d34f093fa21469deb03730929db995bf30c7be85
Stephen <alertbear@163.com>
2015-07-03 21:38:38 +0800

    Stack examples completed

--- basic/example.py
+++ basic/example.py
@@ -48,6 +48,110 @@ def decimal_to_binary(num):
         num //= 2
 
     while not s.isEmpty():
-        binary_str += s.pop()
+        binary_str += str(s.pop())
 
     return binary_str
+
+
+def convert_base(num, base):
+    """
+    Convert decimal number to base number, such as 8 or 16
+    :param num: The raw decimal number
+    :return: the result number
+    """
+    digit = '0123456789abcdef'
+    s = Stack()
+    binary_str = ''
+    while num != 0:
+        rem = num % base
+        s.push(rem)
+        num //= base
+
+    while not s.isEmpty():
+        binary_str += str(digit[s.pop()])
+
+    return binary_str
+
+
+def infix_to_postfix(expr):
+    """
+    Convert the infix expression to postfix one
+    :param expr: raw infix expression
+    :return: result: the converted postfix expression
+    """
+    prec = {'*': 2, '/': 2, '+': 1, '-': 1, '(': 3, ')': 3}
+    index = 0
+    s = Stack()
+    l = []
+    while index < len(expr):
+        # blank " "
+        if expr[index] == ' ':
+            index += 1
+            continue
+        # [*, /, +, -, (, )]
+        if expr[index] in prec.keys():
+            # stack is empty or "("
+            if s.isEmpty() or expr[index] == '(':
+                s.push(expr[index])
+            else:
+                # ")"
+                if expr[index] == ')':
+                    # Pop all the element up of "(" and append to the list
+                    while s.peek() != '(':
+                        l.append(s.pop())
+                    s.pop()  # Pop "("
+                elif not prec[expr[index]] > prec[s.peek()] and s.peek() != '(':
+                    l.append(s.pop())
+                    s.push(expr[index])
+                # Push "(" or the prec of expr[index] lower than s.peek()'s
+                else:
+                    s.push(expr[index])
+        # [abc....012...]
+        else:
+            l.append(expr[index])
+        index += 1
+
+    # Append all remaining operators in stack
+    while not s.isEmpty():
+        l.append(s.pop())
+
+    result = ''.join(l)
+    return result
+
+
+def do_math(op, opr1, opr2):
+    opr1 = int(opr1)
+    opr2 = int(opr2)
+    if op == '*':
+        result = opr1*opr2
+    elif op == '/':
+        result = opr1/opr2
+    elif op == '+':
+        result = opr1 + opr2
+    else:
+        result = opr1 - opr2
+    return result
+
+
+def postfix_result(expr):
+    """
+    Get the result of postfix expression
+    :param expr: postfix expression
+    :return: result
+    """
+    expr_list = expr.split()
+    operators = ['*', '/', '+', '-']
+    s = Stack()
+    index = 0
+    while index < len(expr_list):
+        if not expr_list[index] in operators:
+            s.push(expr_list[index])
+        else:
+            opr2 = s.pop()
+            opr1 = s.pop()
+            temp = do_math(expr_list[index], opr1, opr2)
+            s.push(temp)
+        index += 1
+
+    result = s.pop()
+    return result
--- basic/test.py
+++ basic/test.py
@@ -1,4 +1,4 @@
-from example import is_brackets_blanced
+from example import *
 
 
 def test_is_brackets_blanced():
@@ -9,6 +9,25 @@ def test_is_brackets_blanced():
     print is_brackets_blanced('{{([][])}()}')
     print is_brackets_blanced('[{()]')
 
+def test_decimal_to_binary():
+    print decimal_to_binary(42)
+    print decimal_to_binary(8)
+
+def test_convert_base():
+    print convert_base(10, 8)
+    print convert_base(1211, 16)
+
+
+def test_infix_to_postfix():
+    print infix_to_postfix("a * b + c * ( e - f )")
+    print infix_to_postfix("a + b + c")
+    print infix_to_postfix("a * b + c * d")
+    print infix_to_postfix("( a + b ) * c - ( d - e ) * ( f + g )")
+    print infix_to_postfix("( A + B ) * ( C + D )")
+
+def test_postfix_result():
+    print postfix_result('7 8 + 3 2 + /')
+    print postfix_result('17 10 + 3 * 9 /')
 
 if __name__ == '__main__':
-    test_is_brackets_blanced()
+    test_postfix_result()
