87e3936f0e07123a061721ae2e41aa12c8bb8a59
dguo <dguo@redhat.com>
2017-10-23 17:34:37 +0800

    Add question description to jianzhi

--- jianzhi/010.py
+++ jianzhi/010.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
+# Q:
+# 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+# 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+
+
 class Solution:
-    """
-    我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
-    """
+
     def rectCover(self, number):
         # write code here
         if number <= 0:
@@ -15,7 +18,7 @@ class Solution:
         n1 = 2
         n2 = 1
         while number > 2:
-            n1 = n1 + n2
+            n1 += n2
             n2 = n1 - n2
             number -= 1
 
---
+++ microsoft/README.md
@@ -0,0 +1,3 @@
+
+[Microsoft数据结构算法](http://www.cnblogs.com/v-July-v/archive/2010/11/17/1983725.html)
+
