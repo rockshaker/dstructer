29b7ec9eec3a982cd91808a88a151a37a699459d
dguo <dguo@redhat.com>
2017-10-25 18:59:14 +0800

    pythonds rewrite

---
+++ microsoft/005.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+# 5.查找最小的k 个元素
+# 题目：输入n 个整数，输出其中最小的k 个。
+# 例如输入1，2，3，4，5，6，7 和8 这8 个数字，则最小的4 个数字为1，2，3 和4
+
+class Solution:
+    def findMinK(self, *nums):
+        pass
---
+++ pythonds/linkedlist.py
@@ -0,0 +1,53 @@
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    @property
+    def data(self):
+        return self.data
+
+    @data.setter
+    def data(self, new_data):
+        self.data = new_data
+
+    @property
+    def next(self):
+        return self.next
+
+    @next.setter
+    def next(self, new_next):
+        self.next = new_next
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        temp = ListNode(data)
+        temp.next(self.head)
+        self.head = temp
+
+    def find(self, data):
+        current = self.head
+        found = False
+        while current and not found:
+            if current.data() == data:
+                found = True
+            else:
+                current = current.next()
+        return found
+
+    def remove(self, data):
+        prev = None
+        current = self.head
+        found = False
+        while current and not found:
+            if current.data() == data:
+                found = True
+            else:
+                prev = current
+                current = current.next()
+        # FIX ME 
+        prev.next(current.next())
