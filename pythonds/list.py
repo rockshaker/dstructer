008110cb60a6fad0817ea577939f521a7b527b1c
dguo <dguo@redhat.com>
2017-10-26 18:20:19 +0800

    list fix

--- pythonds/list.py
+++ pythonds/list.py
@@ -3,20 +3,16 @@ class ListNode:
         self.data = data
         self.next = None
 
-    @property
-    def data(self):
+    def get_data(self):
         return self.data
 
-    @data.setter
-    def data(self, new_data):
-        self.data = new_data
+    def set_data(self, data):
+        self.data = data
 
-    @property
-    def next(self):
+    def get_next(self):
         return self.next
 
-    @next.setter
-    def next(self, new_next):
+    def set_next(self, new_next):
         self.next = new_next
 
 
@@ -24,33 +20,118 @@ class UnorderedList:
     def __init__(self):
         self.head = None
 
-    def add(self, data):
-        temp = ListNode(data)
-        temp.next(self.head)
+    def add(self, item):
+        temp = ListNode(item)
+        temp.set_next(self.head)
         self.head = temp
 
-    def find(self, data):
+    def find(self, item):
         current = self.head
         found = False
         while current and not found:
-            if current.data() == data:
+            if current.item() == item:
                 found = True
             else:
-                current = current.next()
+                current = current.get_next()
         return found
 
-    def remove(self, data):
+    def remove(self, item):
         prev = None
         current = self.head
         found = False
         while current and not found:
-            if current.data() == data:
+            if current.item() == item:
                 found = True
             else:
                 prev = current
-                current = current.next()
+                current = current.get_next()
 
         if not prev:
-            self.head = current.next()
+            self.head = current.get_next()
         else:
-            prev.next(current.next())
+            prev.set_next(current.get_next())
+
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def list_print(self):
+        res = []
+        current = self.head
+        while current:
+            res.append(current.get_data())
+            current = current.get_next()
+
+        print res
+
+    def find(self, item):
+        current = self.head
+        stop = False
+        found = False
+
+        while current is not None and not stop and not found:
+            print current
+            if current.get_data() == item:
+                found = True
+            elif current.get_data() > item:
+                stop = True
+            else:
+                current = current.get_next()
+
+        return found
+
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+
+        while current is not None and not stop:
+            if current.get_data() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        temp = ListNode(item)
+        if previous is None:
+            temp.set_next(current)
+            self.head = temp
+        else:
+            temp.set_next(current)
+            previous.set_next(temp)
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        stop = False
+
+        while current is not None and not found and not stop:
+            if current.get_data() == item:
+                found = True
+            elif current.get_data() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if found:
+            if previous is None:
+                self.head = current.get_next()
+            else:
+                previous.set_next(current.get_next)
+
+
+if __name__ == "__main__":
+    ol = OrderedList()
+    ol.add(1)
+    ol.list_print()
+
+    ol.add(0)
+    ol.list_print()
+
+    print "found" if ol.find(1) else "not found"
+
+    ol.remove(1)
+    print "found" if ol.find(1) else "not found"
